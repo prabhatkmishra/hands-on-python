fd24707598733c91344f2b64134489cc3a13edc5
Prabhat Kumar Mishra <29929866+prabhatkmishra@users.noreply.github.com>
2023-03-23 08:30:36 +0000

    new file is created to start the repo

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -1,12 +1,12 @@
 greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
 
-name = "John"
+# name = "John"
 
-intrupution = f"Hello {name}"
+# intrupution = f"Hello {name}"
 
-greet_format = "Hello {}"
+# greet_format = "Hello {}"
 
-formatted = greet_format.format(name)
+# formatted = greet_format.format(name)
 
-print(intrupution, formatted)
+# print(intrupution, formatted)
